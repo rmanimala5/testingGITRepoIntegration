2fb3ac511b9f2eb05e9460cd8047e43946e101bb
manimala ramesh <rmanimala5@gmail.com>
2023-05-13 10:34:01 +0530

    my first commit in git repo

---
+++ eclipse-workspace-202206v/test_artifactID1/test_artifactID1-backend/src/test/java/test_groupID1/test_artifactID1/AppTest.java
@@ -0,0 +1,12 @@
+package test_groupID1.test_artifactID1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
